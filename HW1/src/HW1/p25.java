3fddf94864c179160c4a6055a9020cc9d5f9d97b
kitopboboi <101378237+kitopboboi@users.noreply.github.com>
2023-10-26 20:34:25 +0800

    Add files via upload

---
+++ HW1/src/HW1/p25.java
@@ -0,0 +1,15 @@
+package HW1;
+
+public class p25 {
+
+	public static void main(String[] args) {
+		int num;
+		
+		num = 3;
+		
+		System.out.print("變數num的值是" + num);
+	
+
+	}
+
+}
---
+++ HW1/src/HW1/p27.java
@@ -0,0 +1,20 @@
+package HW1;
+
+public class p27 {
+
+	public static void main(String[] args) {
+		
+		int num1, num2;
+		
+		num1 = 3;
+		
+		System.out.println("變數num的值是" + num1);
+		
+		num2 = num1;
+		
+		System.out.println("將變數num1指定到變數num2之中");
+		System.out.println("變數num更新後的值是" + num2);
+
+	}
+
+}
